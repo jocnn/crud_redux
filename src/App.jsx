845ffb5e9809d978356614f69457192434cf48b5
jocnn <ijann_@outlook.com>
2022-06-04 10:23:37 -0300

    agregando rutas a los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Header from "./components/Header"
+import Products from "./components/Products"
+import NewProduct from "./components/NewProduct"
+import EditProduct from "./components/EditProduct"
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+
+      <div className="container mt-5">
+        <Routes path="/">
+          <Route path="/" element={ <Products /> } />
+          <Route path="/productos" >
+            <Route index element={ <Products /> } />
+            <Route path="nuevo" element={ <NewProduct /> } />
+            <Route path="editar/:id" element={ <EditProduct /> } />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 

2ce681a1543beacb407283022613636cc40059e2
jocnn <ijann_@outlook.com>
2022-06-04 20:12:45 -0300

    configurando rutas, el provider y store de redux

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,26 @@ import Products from "./components/Products"
 import NewProduct from "./components/NewProduct"
 import EditProduct from "./components/EditProduct"
 
+import { Provider } from 'react-redux'
+import store from "./store"
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <Provider store={store}>
+        <Header />
 
-      <div className="container mt-5">
-        <Routes path="/">
-          <Route path="/" element={ <Products /> } />
-          <Route path="/productos" >
-            <Route index element={ <Products /> } />
-            <Route path="nuevo" element={ <NewProduct /> } />
-            <Route path="editar/:id" element={ <EditProduct /> } />
-          </Route>
-        </Routes>
-      </div>
+        <div className="container mt-5">
+          <Routes path="/">
+            <Route path="/" element={ <Products /> } />
+            <Route path="/productos" >
+              <Route index element={ <Products /> } />
+              <Route path="nuevo" element={ <NewProduct /> } />
+              <Route path="editar/:id" element={ <EditProduct /> } />
+            </Route>
+          </Routes>
+        </div>
+      </Provider>
     </BrowserRouter>
   )
 }

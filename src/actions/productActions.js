55e4ea275c7c57e64290347b582494f95b951ff2
jocnn <ijann_@outlook.com>
2022-06-05 13:50:30 -0300

    definiendo las actions del producto

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -5,7 +5,28 @@ import {
 } from "../types";
 
 export function crearNuevoProductoAccion(producto) {
-  return () => {
-    console.log(producto)
+  return (dispatch) => {
+    dispatch(agregarProducto())
+
+    try {
+      dispatch(agregarProductoExito(producto))
+    } catch (error) {
+      dispatch(agregarProductoError(true))
+    }
   }
 }
+
+const agregarProducto = () => ({
+  type: PRODUCTO_AGREGAR,
+  payload: true
+})
+
+const agregarProductoExito = producto => ({
+  type: PRODUCTO_AGREGAR_EXITO,
+  payload: producto
+})
+
+const agregarProductoError = estado => ({
+  type: PRODUCTO_AGREGAR_ERROR,
+  payload: estado
+})

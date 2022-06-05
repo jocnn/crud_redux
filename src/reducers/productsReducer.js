7c5e3c6ee5a10e0c151e037194658b6658909b01
jocnn <ijann_@outlook.com>
2022-06-05 09:49:43 -0300

    importando los types en el reducer

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,12 +1,18 @@
-const initialState = {
-  products: [],
-  error: null,
-  loading: false
-}
+import {
+	PRODUCTO_AGREGAR,
+	PRODUCTO_AGREGAR_EXITO,
+	PRODUCTO_AGREGAR_ERROR,
+} from "../types";
 
-export default function(state = initialState, action) {
-  switch(action.type) {
-    default:
-      return state
-  }
+const initialState = {
+	products: [],
+	error: null,
+	loading: false,
+};
+
+export default function (state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
 }

f0f1053f51615ece5bb1f2a2faedb3f3184a19ff
jocnn <ijann_@outlook.com>
2022-06-05 13:49:47 -0300

    definiendo el reducer de productos

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -11,8 +11,25 @@ const initialState = {
 };
 
 export default function (state = initialState, action) {
-	switch (action.type) {
-		default:
+	switch(action.type) {
+		case PRODUCTO_AGREGAR:
+      return {
+        ...state,
+        loading: action.payload
+      }
+    case PRODUCTO_AGREGAR_EXITO:
+      return {
+        ...state,
+        loading: false,
+        products: [...state.products, action.payload]
+      }
+    case PRODUCTO_AGREGAR_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      }
+    default:
 			return state;
 	}
 }

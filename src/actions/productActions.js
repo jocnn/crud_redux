73491278574bea6aa5c7b0992e33f3bd27ddba18
jocnn <ijann_@outlook.com>
2022-06-06 12:37:58 -0300

    agregando alertas Swal

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,6 +4,8 @@ import {
 	PRODUCTO_AGREGAR_ERROR,
 } from "../types";
 import clienteAxios from "../config/axios";
+import Swal from "sweetalert2"
+import 'sweetalert2/dist/sweetalert2.css'
 
 export function crearNuevoProductoAccion(producto) {
   return async (dispatch) => {
@@ -12,9 +14,22 @@ export function crearNuevoProductoAccion(producto) {
     try {
       await clienteAxios.post('/productos', producto)
       dispatch(agregarProductoExito(producto))
+
+      Swal.fire(
+        'Correcto',
+        'El producto se agrego correctamente',
+        'success'
+      )
+
     } catch (error) {
       console.log(error)
       dispatch(agregarProductoError(true))
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Hubo un error',
+        text: 'Hubo un error, intenta de nuevo'
+      })
     }
   }
 }

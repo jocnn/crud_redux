c14e37c2ab6fed731911b88c0da9132f23e4bd2c
jocnn <ijann_@outlook.com>
2022-06-06 20:14:09 -0300

    mostrando lista de productos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,18 +3,21 @@ import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { obtenerProductosAction } from "../actions/productActions"
 
+import Product from "./Product"
+
 const Products = () => {
 
   const dispatch = useDispatch()
 
   useEffect(() => {
-    
     // consultar API
     const cargarProductos = () => dispatch(obtenerProductosAction())
     cargarProductos()
-
   }, [])
 
+  const productos = useSelector((state) => state.products.products)
+
+  
   return (
     <>
       <h2
@@ -26,13 +29,20 @@ const Products = () => {
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
           <tr>
-            <td scope="col">Nombre</td>
-            <td scope="col">Precio</td>
-            <td scope="col">Acciones</td>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
           </tr>
         </thead>
         <tbody>
-          
+          {
+            productos.map( producto => (
+              <Product 
+                key={producto.id}
+                producto={producto}
+              />
+            ))
+          }
         </tbody>
       </table>
     </>
